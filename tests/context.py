cbad97a54e0318ea4c5d95bc78bfbba6207676c2
gcontarini <>
2020-12-02 12:52:04 +0000

    Create context file and first test

---
+++ tests/context.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+# Add path modules
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../indicators')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../kpis')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../data-scrapper')))
+
+# Add functions/scripts
+import macd
